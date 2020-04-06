1b1c86cacfd35b6cfb8b7b6aa5399446d84aeca6
Sindre Haavaldsen <sindre.h@outlook.com>
2020-04-06 16:38:07 +0200

    Add InfoBox component to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import Packs from './Packs'
+import InfoBoxes from './InfoBoxes'
 import '../css/homepage.css';
 
 export default class Homepage extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            welcomeTitle: "Velkommen til SH Bilpleie",
+            welcomeParagraph: "Nisi culpa minim reprehenderit sint ut quis voluptate cillum laborum. Ex ad aliquip esse do commodo minim fugiat mollit commodo. Labore id dolore sit sint anim adipisicing. Nisi consequat commodo laborum excepteur pariatur proident non ex amet proident. Veniam dolore consequat sunt anim adipisicing dolore nisi veniam voluptate dolore duis commodo. Veniam sit et enim proident aute exercitation aute veniam. "
+        }
+    }
+
     render() {
         return (
             <div className="homepage">
-                <Packs/>
+                <section className="homepage-welcome">
+                    <h1>
+                        {this.state.welcomeTitle}
+                    </h1>
+                    <p>
+                        {this.state.welcomeParagraph}
+                    </p>
+                </section>
+
+                <div className="splitter"/>
+
+                <section className="infoBoxes">
+                    <InfoBoxes/>
+                </section>
             </div>
         )
     }
---
+++ src/components/InfoBoxes.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import '../css/infoBoxes.css';
+
+export default class InfoBoxes extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            infoBoxes: [
+                {
+                    title: "En fornøyd bil",
+                    description: "Gi bilen den gaven den fortjener etter å frakte deg fra A - Å på en daglig basis. Ikke bare vil du selv være fornøyd med behandlingen, men bilen din vil takke deg og se blank ut i lang tid fremmover",
+                    icon: "fas fa-smile"
+                },
+                {
+                    title: "Jevnlig vedlikehold",
+                    description: "Ved å jevnlig vedlikeholde lakken og interiøret til bilen vil den ikke bare se bra ut, men også holde seg i bedre stand over lenger tid. Med polering og voksing vil lakken holde seg beskyttet mot skader fra blandt annet sollys og salt eller asfalt tjære",
+                    icon: "fas fa-calendar-alt"
+                },
+                {
+                    title: "Ferdig på dagen",
+                    description: "Jeg garanterer ferdig behandling av bilen på under en dag på alle pakker. Lever bilen på morgenen og ha den tilbake senest på kveldstid",
+                    icon: "fas fa-clock"
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            this.state.infoBoxes.map((info, i) => (
+                <div class="infoBox">
+                    <i className={info.icon}/>
+                    <h1>
+                        {info.title}
+                    </h1>
+                    <p>
+                        {info.description}
+                    </p>
+                </div>
+            ))
+        )
+    }
+}
