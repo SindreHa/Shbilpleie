0e42ebebfad67be69b3284c2142398347fd44c29
Sindre Haavaldsen <sindre.h@outlook.com>
2020-04-11 16:20:01 +0200

    Added extra packs

--- src/components/Packs.js
+++ src/components/Packs.js
@@ -24,7 +24,7 @@ export default class Packs extends Component {
                 {
                     style: "wash",
                     title: "Utvendig vask",
-                    description: "Skånsom og grundig utvendig vask som fjerner salt- og asfaltpartikler fra lakk og felger",
+                    description: "Skånsom vedlikeholdsvask som vasker i dybden uten å fjerne eksiterende beskyttelse. Inkluderer også vask av felger",
                     price: "250,-"
                 },
                 {
@@ -36,26 +36,40 @@ export default class Packs extends Component {
                 {
                     style: "wash",
                     title: "Innvendig og utvendig vask",
-                    description: "Både innvendig og utvendig vask. Skånsom og grundig utvendig vask som fjerner salt- og asfaltpartikler fra lakk og felger. Grundig og detaljert vask og rens av interiør. Gummifornyer blir påført på dashboard og andre deler av plastikk/gummi for å gjenopprette den opprinnelige overflate-farge og etterlate et klart, beskyttende belegg",
+                    description: "Både innvendig og utvendig vask. Skånsom vedlikeholdsvask som vasker i dybden uten å fjerne eksiterende beskyttelse. Inkluderer også vask av felger. Grundig og detaljert vask og rens av interiør. Gummifornyer blir påført på dashboard og andre deler av plastikk/gummi for å gjenopprette den opprinnelige overflate-farge og etterlate et klart, beskyttende belegg",
                     price: "500,-"
                 },
                 {
                     style: "wash-polish",
                     title: "Utvendig vask m/voksing",
                     description: "Skånsom og grundig utvendig vask som fjerner salt- og asfaltpartikler fra lakk og felger etterfulgt av et høykvalitets vokslag for beskyttelse og høy glans",
-                    price: "800,-"
+                    price: "1000,-"
                 },
                 {
                     style: "wash-polish",
                     title: "Utvendig og innvendig vask m/voksing",
                     description: "Skånsom og grundig utvendig vask som fjerner salt- og asfaltpartikler fra lakk og felger etterfulgt av et høykvalitets vokslag for beskyttelse og høy glans. Grundig og detaljert vask og rens av interiør. Gummifornyer blir påført på dashboard og andre deler av plastikk/gummi for å gjenopprette den opprinnelige overflate-farge og etterlate et klart, beskyttende belegg",
-                    price: "1000,-"
+                    price: "1200,-"
                 },
                 {
                     style: "wash-polish",
-                    title: "Utvendig og innvendig vask m/rubbing og voksing",
+                    title: "Utvendig og innvendig vask m/lakk korrigering, polering og voksing",
                     description: "Skånsom og grundig utvendig vask som fjerner salt- og asfaltpartikler fra lakk og felger. Fjerning av vaskeriper og oksidasjon i lakken etterfulgt av polering og et høykvalitets vokslag for beskyttelse og høy glans. Grundig og detaljert vask og rens av interiør. Gummifornyer blir påført på dashboard og andre deler av plastikk/gummi for å gjenopprette den opprinnelige overflate-farge og etterlate et klart, beskyttende belegg",
-                    price: "1500,-"
+                    price: "2000,-"
+                }
+            ],
+            extraPacks : [
+                {
+                    title: "Hjulskift",
+                    description: "Bytting av sommerhjul/vinterhjul. Blir gjort med momentnøkkel for korrekt tilstramming av bolter",
+                    price: "250,-",
+                    style: "extra"
+                },
+                {
+                    title: "Vask av hjul",
+                    description: "Grundig vask av hjul som fjerner alt av salt- og asfaltpartikler",
+                    price: "250,-",
+                    style: "extra"
                 }
             ]
         }
@@ -76,8 +90,8 @@ export default class Packs extends Component {
                     this.state.packs.map((pack, i) => {
                         delay += 70
                         return(
-                        <SlideIn in={this.state.transIn} delay={delay}>
-                        <div className={"pack " + pack.style} key={i}>
+                        <SlideIn in={this.state.transIn} delay={delay} key={i}>
+                        <div className={"pack " + pack.style}>
                             <h1>{pack.title}</h1>
                             <h1 className="priceTitle">Pris</h1>
                             <p>{pack.description}</p>
@@ -86,6 +100,23 @@ export default class Packs extends Component {
                         </SlideIn>
                     )}) 
                 }
+                <h1 style={{textAlign: "center"}}>Ekstrapakker</h1>
+                <div id="extraPacks">
+                {
+                    this.state.extraPacks.map((pack, i) => {
+                        delay += 70
+                        return(
+                        <SlideIn in={this.state.transIn} delay={delay} key={i}>
+                        <div className={"pack " + pack.style}>
+                            <h1>{pack.title}</h1>
+                            <h1 className="priceTitle">Pris</h1>
+                            <p>{pack.description}</p>
+                            <p className="price">{pack.price}</p>
+                        </div>
+                        </SlideIn>
+                    )}) 
+                }
+                </div>
                 </section>
             </div>
         )
